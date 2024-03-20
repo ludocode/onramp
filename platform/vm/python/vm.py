27f45a7c90d7260249d0a35d632dd4470f602c18
Nicholas Fraser <nick@ludocode.com>
2024-03-20 00:12:58 -0400

    vm/python: Optimize Python VM

--- platform/vm/python/vm.py
+++ platform/vm/python/vm.py
@@ -26,9 +26,6 @@
 
 # This is an Onramp VM implemented in Python.
 #
-# Memory is stored as an array of 4K pages starting at the base address. Each
-# page is None until accessed.
-#
 # All values are unsigned. Registers store 32-bit unsigned values. All
 # functions (including parseMix()) return unsigned values.
 #
@@ -37,21 +34,15 @@
 # will result in a Python exception. Other errors (such as unaligned
 # load/store) may work for some addresses and not others.
 #
-# Despite this, the Python VM is around 100x slower than the C89 VM. I have
-# some ideas for how to improve this. The obvious one is to eliminate paging.
-#
-# We could try simply creating a 16MB bytearray on startup. We could also try
-# memoryview to read words and bytes rather than using struct and slices.
-#
-# Another option is to try a plain list of words instead of an array, e.g.
-# `memory = [0]*2**22`. This doubles the memory usage but avoids the
-# boxing/unboxing penalty of array values. Since most stores and loads are
-# words it's probably best to avoid struct/memoryview to access them.
+# There are a handful of optimizations below, some of which compromise
+# readability. Nevertheless, this VM is about 50x slower than a handwritten
+# machine code VM, and about 100x slower than a VM in a compiled language.
+# This is probably as good as it will get. Python is just very slow
+# unfortunately.
 
 
 
 import sys, os, struct, traceback, time
-#import cProfile
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -59,84 +50,40 @@ def eprint(*args, **kwargs):
 def fatal(message, address=None):
     raise Exception(message)
 
-# debug tracing
-traceEnabled = False
-traceDepth = 0
-def traceInc():
-    if traceEnabled:
-        global traceDepth
-        traceDepth += 1
-def traceDec():
-    if traceEnabled:
-        global traceDepth
-        traceDepth -= 1
-def trace(*args, **kwargs):
-    if traceEnabled:
-        eprint("  " * traceDepth, end='')
-        eprint(*args, **kwargs)
-
 # register names
 RSP = 0xC
 RFP = 0xD
 RPP = 0xE
 RIP = 0xF
 
-# memory layout
-BASE_ADDR = 0x10000
-PAGE_SIZE = 4096
-TOTAL_PAGES = 8192 # 32 MB
-
 # vm state
-pages = [None] * TOTAL_PAGES
+memory = bytearray(2**24)
 registers = [0] * 16
 handles = [sys.stdin.buffer, sys.stdout.buffer, sys.stderr.buffer] + [None] * 13
 
-# Sets a register to a given value, clamping it to 32-bit unsigned.
-def setRegister(register, value):
-    #if value < 0:
-    #    value += 2**32
-    #value &= 0xFFFFFFFF
-    #trace("setting register",register,"to",value,hex(value))
-    #if (register == 0xF):
-        #trace(f"\nCALL/RET {value - BASE_ADDR} hex:{hex(value - BASE_ADDR)}\n")
-    registers[register] = value & 0xFFFFFFFF
+# memory layout
+BASE_ADDR = 0x10000
+MEMORY_SIZE = len(memory)
 
-def getPage(address):
-    address &= 0xFFFFFFFF
-    #if address < BASE_ADDR or address >= BASE_ADDR + len(pages) * PAGE_SIZE:
-    #    raise Exception("Memory address out of range: " + str(address) + " " + hex(address))
-    pageNum = (address - BASE_ADDR) // PAGE_SIZE
-    if pages[pageNum] is None:
-        pages[pageNum] = bytearray(PAGE_SIZE)
-    return pages[pageNum]
+# error codes
+VM_ERR_GENERIC     = 0xFFFFFFFF
+VM_ERR_PATH        = 0xFFFFFFFE
+VM_ERR_IO          = 0xFFFFFFFD
+VM_ERR_UNSUPPORTED = 0xFFFFFFFC
 
 def loadByte(address):
-    return getPage(address)[(address - BASE_ADDR) % PAGE_SIZE]
+    return memory[(address & 0xFFFFFFFF) - BASE_ADDR]
 
-def storeByte(address, byte):
-    getPage(address)[(address - BASE_ADDR) % PAGE_SIZE] = byte
+def storeByte(address, value):
+    memory[(address & 0xFFFFFFFF) - BASE_ADDR] = (value & 0xFF)
 
 def loadWord(address):
-    #if (address % 4) != 0:
-    #    raise Exception("Unaligned load at " + str(address))
-    offset = (address - BASE_ADDR) % PAGE_SIZE
-    return struct.unpack("<I", getPage(address)[offset:offset+4])[0]
-#
-#    value = page[offset] | (page[offset + 1] << 8) | \
-#            (page[offset + 2] << 16) | (page[offset + 3] << 24)
-#    #trace("loaded",value,"(hex)",hex(value),"at",address)
-#    return value
+    address = ((address & 0xFFFFFFFF) - BASE_ADDR)
+    return struct.unpack("<I", memory[address:address+4])[0]
 
 def storeWord(address, value):
-    #if (address % 4) != 0:
-    #    raise Exception("Unaligned store at " + str(address))
-    #trace("storing word",value,"(hex)",hex(value),"at",address)
-    offset = (address - BASE_ADDR) % PAGE_SIZE
-    getPage(address)[offset:offset+4] = struct.pack("<I", value)
-#    page[offset] = value & 0xFF
-#    page[offset + 1] = (value >> 8) & 0xFF
-#    page[offset + 2] = (value >> 16) & 0xFF
-#    page[offset + 3] = (value >> 24) & 0xFF
+    address = ((address & 0xFFFFFFFF) - BASE_ADDR)
+    memory[address:address+4] = struct.pack("<I", value)
 
 def loadString(address):
     s = bytearray()
@@ -161,15 +108,6 @@ def parseRegister(value):
         return value & 0xF
     raise Exception("Argument is not a register")
 
-# Parses the arguments to an instruction where the first argument is the
-# destination register and the next two arguments are mix-type source bytes.
-# This is the format used by most opcodes.
-def parseBinaryOp():
-    dest = parseRegister(loadByte(registers[RIP] - 3))
-    left = parseMix(loadByte(registers[RIP] - 2))
-    right = parseMix(loadByte(registers[RIP] - 1))
-    return dest, left, right
-
 def syscall(number):
     if number == 0x00:  # halt
         sys.exit(registers[0])
@@ -193,9 +131,10 @@ def syscall(number):
                     handles[i] = open(loadString(registers[0]), registers[1] and "wb" or "rb")
                     registers[0] = i
                 except FileNotFoundError:
-                    setRegister(0, -1)
+                    registers[0] = VM_ERR_PATH
                 return
-        raise Exception("No free file handles")
+        registers[0] = VM_ERR_GENERIC
+        return
 
     if number == 0x04:  # fclose
         # Shouldn't be able to close standard streams, we don't bother to check
@@ -209,17 +148,23 @@ def syscall(number):
         count = registers[2]
         i = 0
         while i < count:
-            b = file.read(1)
+            b = file.read(count - i)
+            # TODO try to handle errors gracefully. A read at EOF will return a
+            # size of 0 which we handle correctly but a read error will throw
+            # an exception. For now we let it take down the whole VM.
             if not b:
                 break
-            storeByte(address + i, b[0])
-            i += 1
+            for j in range(len(b)):
+                storeByte(address + i + j, b[j])
+            i += len(b)
         registers[0] = i
         return
 
     if number == 0x06:  # fwrite
-        for i in range(registers[2]):
-            handles[registers[0]].write(bytes([loadByte(registers[1] + i)]))
+        addr = registers[1] - BASE_ADDR
+        handles[registers[0]].write(memory[addr:addr + registers[2]])
+        # TODO try to handle errors gracefully. For now a write error takes
+        # down the whole VM.
         registers[0] = registers[2]
         return
 
@@ -230,7 +175,7 @@ def syscall(number):
         if offset >= 2**63:
             offset = offset - 2**64
         file.seek(offset, base)
-        setRegister(0, 0)
+        registers[0] = 0
         return
 
     if number == 0x08:  # ftell
@@ -238,122 +183,134 @@ def syscall(number):
         addr = registers[1]
         storeWord(addr, position)
         storeWord(addr + 4, position >> 32)
-        setRegister(0, 0)
+        registers[0] = 0
         return
 
     if number == 0x09:  # ftrunc
         handles[registers[0]].truncate(registers[1] | (registers[2] << 32))
-        setRegister(0, 0)
+        registers[0] = 0
         return
 
     if number == 0x10:  # unlink
         try:
             os.remove(loadString(registers[0]))
-            setRegister(0, 0)
+            registers[0] = 0
         except:
-            setRegister(0, 0xFFFFFFFF)
+            registers[0] = VM_ERR_GENERIC
         return
 
     if number == 0x11:  # chmod
         try:
             os.chmod(loadString(registers[0]), registers[1])
-            setRegister(0, 0)
+            registers[0] = 0
         except:
-            setRegister(0, 0xFFFFFFFF)
+            registers[0] = VM_ERR_GENERIC
         return
 
     raise Exception("Invalid or unimplemented syscall: " + str(number))
 
-def step():
-    opcode = loadByte(registers[RIP])
-    registers[RIP] += 4
-    #trace("")
-    #trace(hex(256+opcode)[3:].upper(),
-    #      hex(256+loadByte(registers[RIP] - 3))[3:].upper(),
-    #      hex(256+loadByte(registers[RIP] - 2))[3:].upper(),
-    #      hex(256+loadByte(registers[RIP] - 1))[3:].upper())
-    #trace("step", "opcode",hex(opcode)[2:],"reg",registers)
+def run():
 
-    if (opcode & 0xF0) != 0x70:
-        raise Exception("Invalid opcode: " + str(opcode) + " addr " + str(registers[RIP]-4))
-    opcode = opcode & 0xF
-    mask = 1 << opcode
+    # These local aliases seem to improve performance
+    memory = globals()["memory"]
+    registers = globals()["registers"]
+    parseMix = globals()["parseMix"]
+    parseRegister = globals()["parseRegister"]
+    loadWord = globals()["loadWord"]
+    loadByte = globals()["loadByte"]
+    storeWord = globals()["storeWord"]
+    storeByte = globals()["storeByte"]
+    syscall = globals()["syscall"]
 
-    # Eleven basic instructions share the same argument types: they all take a
-    # destination register and two mix-type arguments. We handle them all
-    # together here.
-    if mask & 0b0010010111111111:
-        dest, left, right = parseBinaryOp()
-        #trace(f"{hex(dest)} = {left} <op> {right},  opcode: {hex(0x70+opcode)[2:]}")
-        if opcode == 0: # add
-            setRegister(dest, left + right)
-        elif opcode == 1: # sub
-            setRegister(dest, left - right)
-        elif opcode == 2: # mul
-            setRegister(dest, left * right)
-        elif opcode == 3: # div
-            setRegister(dest, int(left / right))
-        elif opcode == 4: # and
-            setRegister(dest, left & right)
-        elif opcode == 5: # or
-            setRegister(dest, left | right)
-        elif opcode == 6: # xor
-            setRegister(dest, left ^ right)
-        elif opcode == 7: # ror
-            right &= 0x1F
-            setRegister(dest, 0xFFFFFFFF &
-                    (left >> right) | (left << ((-right) & 0x1F)))
-        elif opcode == 8: # ldw
-            setRegister(dest, loadWord(left + right))
-        elif opcode == 0xA: # ldb
-            setRegister(dest, loadByte(left + right))
-        elif opcode == 0xD: # cmpu
-            if left < right:
-                setRegister(dest, -1)
-            elif left > right:
-                setRegister(dest, 1)
+    # We've also inlined the loadByte() function in a handful of places below,
+    # basically anywhere you see BASE_ADDR. It's much quicker to access memory
+    # and registers directly.
+
+    while True:
+
+        opcode = memory[registers[RIP] - BASE_ADDR]
+        registers[RIP] += 4
+
+        if (opcode & 0xF0) != 0x70:
+            raise Exception("Invalid opcode: " + str(opcode) + " addr " + str(registers[RIP]-4))
+        opcode = opcode & 0xF
+        mask = 1 << opcode
+
+        # Eleven basic instructions share the same argument types: they all take a
+        # destination register and two mix-type arguments. We handle them all
+        # together here.
+        if mask & 0b0010010111111111:
+            dest = parseRegister(memory[registers[RIP] - BASE_ADDR - 3])
+            left = parseMix(memory[registers[RIP] - BASE_ADDR - 2])
+            right = parseMix(memory[registers[RIP] - BASE_ADDR - 1])
+            if opcode == 0: # add
+                registers[dest] = (left + right) & 0xFFFFFFFF
+            elif opcode == 1: # sub
+                registers[dest] = (left - right) & 0xFFFFFFFF
+            elif opcode == 2: # mul
+                registers[dest] = (left * right) & 0xFFFFFFFF
+            elif opcode == 3: # divu
+                registers[dest] = int(left / right)
+            elif opcode == 4: # and
+                registers[dest] = left & right
+            elif opcode == 5: # or
+                registers[dest] = left | right
+            elif opcode == 6: # xor
+                registers[dest] = left ^ right
+            elif opcode == 7: # ror
+                right &= 0x1F
+                registers[dest] = (0xFFFFFFFF &
+                        ((left >> right) | (left << ((-right) & 0x1F))))
+            elif opcode == 8: # ldw
+                registers[dest] = loadWord(left + right)
+            elif opcode == 0xA: # ldb
+                registers[dest] = loadByte(left + right)
+            elif opcode == 0xD: # cmpu
+                if left < right:
+                    registers[dest] = 0xFFFFFFFF
+                elif left > right:
+                    registers[dest] = 1
+                else:
+                    registers[dest] = 0
             else:
-                setRegister(dest, 0)
+                raise Exception("Internal error")
 
-    # Our two store instructions take three mix-type arguments.
-    elif mask & 0b0000101000000000:
-        value = parseMix(loadByte(registers[RIP] - 3))
-        base = parseMix(loadByte(registers[RIP] - 2))
-        offset = parseMix(loadByte(registers[RIP] - 1))
-        if opcode == 9: # stw
-            storeWord(base + offset, value)
-        elif opcode == 0xB: # stb
-            storeByte(base + offset, value & 0xFF)
-
-    # Immediate short and jump if zero both take two literal bytes.
-    elif mask & 0b0101000000000000:
-        value = loadByte(registers[RIP] - 2) | (loadByte(registers[RIP] - 1) << 8)
-        if opcode == 0xC: # ims
-            dest = parseRegister(loadByte(registers[RIP] - 3))
-            setRegister(dest, (registers[dest] << 16) & 0xFFFFFFFF | value)
-        elif opcode == 0xE: # jz
-            pred = parseMix(loadByte(registers[RIP] - 3))
-            if pred == 0:
-                if value > 0x7FFF:
-                    #value |= 0xFFFF0000
-                    value -= 0x10000
-                #trace("jumping by " + str(value) + " * 4")
-                #setRegister(RIP, registers[RIP] + value * 4)
-                registers[RIP] += value * 4
+        # Our two store instructions take three mix-type arguments.
+        elif mask & 0b0000101000000000:
+            value = parseMix(memory[registers[RIP] - BASE_ADDR - 3])
+            base = parseMix(memory[registers[RIP] - BASE_ADDR - 2])
+            offset = parseMix(memory[registers[RIP] - BASE_ADDR - 1])
+            if opcode == 9: # stw
+                storeWord(base + offset, value)
+            elif opcode == 0xB: # stb
+                storeByte(base + offset, value & 0xFF)
             else:
-                pass##trace("pred is not zero, not jumping")
+                raise Exception("Internal error")
 
-    # System call takes only a single argument, the syscall number. The other
-    # arguments must be zero.
-    elif opcode == 0xF: # sys
-        arg2 = loadByte(registers[RIP] - 2)
-        arg3 = loadByte(registers[RIP] - 1)
-        if arg2 != 0 or arg3 != 0:
-            raise Exception("Extra arguments to syscall instruction must be 0.")
-        syscall(loadByte(registers[RIP] - 3))
+        # Immediate short and jump if zero both take two literal bytes.
+        elif mask & 0b0101000000000000:
+            value = memory[registers[RIP] - BASE_ADDR - 2] | \
+                    (memory[registers[RIP] - BASE_ADDR - 1] << 8)
+            arg1 = memory[registers[RIP] - BASE_ADDR - 3]
+            if opcode == 0xC: # ims
+                dest = parseRegister(arg1)
+                registers[dest] = ((registers[dest] << 16) & 0xFFFFFFFF) | value
+            elif opcode == 0xE: # jz
+                if 0 == parseMix(arg1):
+                    if value > 0x7FFF:
+                        #value |= 0xFFFF0000
+                        value -= 0x10000
+                    registers[RIP] += value * 4
+            else:
+                raise Exception("Internal error")
 
-    else:
-        raise Exception()
+        # System call takes only a single argument, the syscall number. The other
+        # arguments must be zero (but we don't bother to check.)
+        elif opcode == 0xF: # sys
+            syscall(loadByte(registers[RIP] - 3))
+
+        else:
+            raise Exception("Internal error")
 
 def start():
 
@@ -411,7 +368,7 @@ def start():
     registers[RPP] = programAddress
     registers[RIP] = programAddress
     registers[0] = tableAddress
-    registers[RSP] = BASE_ADDR + PAGE_SIZE * TOTAL_PAGES
+    registers[RSP] = BASE_ADDR + MEMORY_SIZE
 
     # Fill process info table
     storeWord(tableAddress, 0) # version
@@ -425,13 +382,10 @@ def start():
     storeWord(tableAddress + 32, dirAddress)  # working directory
     storeWord(tableAddress + 36, 0)  # capabilities
 
-    # Run
-    while True:
-        step()
+    run()
 
 if __name__ == "__main__":
     try:
-        #cProfile.run("start()")
         start()
     except SystemExit:
         raise
