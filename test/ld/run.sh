4a781132b86f83b999a9c89918f3e73187f1f935
Nicholas Fraser <nick@ludocode.com>
2025-07-25 18:11:32 -0700

    test/ld: Remove generated output for some tests
    Tests no longer require matching to a generated output file. Instead a
    .fail file is used to mark failing tests, the same as all the other
    tools.
    
    This fixes the problem of failing ld tests every time the libc changes
    (see the parent commit.)
    
    An eventual goal is to transition away from having any generated test
    output in the repository. This is more difficult for the linker because
    it produces binary output. Many of the test cases will need to be
    rewritten to be runnable programs.

--- test/ld/run.sh
+++ test/ld/run.sh
@@ -9,17 +9,25 @@
 #
 #     test/ld/run.sh test/ld/0-global onrampvm build/intermediate/ld-0-global/ld.oe
 #
+# - If a corresponding .fail file exists, the linker must return an error. Its
+# output is ignored.
+#
 # - If a corresponding .oe file exists, the linker must succeed and its output
 # must match the file's contents.
 #
-# - If no corresponding .oe file exists, the linker must return an error. Its
+# - If no corresponding .oe or .fail file exists, the linker must succeed. Its
 # output is ignored.
 #
-# - If a corresponding .args file exists, the contents are passed as command-line
-# arguments to the linker.
+# - If a corresponding .skip file exists, the test is skipped.
 #
-# - If a corresponding .stdout file exists, the resulting program is run, and the
-# output must match the file's contents.
+# - If a corresponding .args file exists, the contents are passed as
+# command-line arguments to the linker instead of the default arguments. Use
+# $INPUT for the input filename, $OUTPUT for the output filename, and $LIBC for
+# libc/0.
+#
+# - If a corresponding .stdout file exists, the program is run. The program
+# must exit with status 0 and the output of running the program must match the
+# file's contents.
 
 if [ "$1" == "" ]; then
     echo "Need folder to test."
@@ -58,7 +66,7 @@ shift
 COMMAND="$@"
 TEMP_OE=$ONRAMP_TMPDIR/onramp-test.oe
 TEMP_STDOUT=$ONRAMP_TMPDIR/onramp-test.stdout
-ERROR=0
+ANY_ERROR=0
 
 # Some tests link against the libc. They provide arguments that use it here. We
 # use libc/0 because early linker stages don't have the capacity to link the
@@ -70,8 +78,14 @@ TESTS_PATH="$(basename $(realpath $SOURCE_FOLDER/..))/$(basename $(realpath $SOU
 echo "Running $TESTS_PATH tests on: $COMMAND"
 
 for TESTFILE in $(find $SOURCE_FOLDER/* -name '*.oo'); do
-    echo "Testing $TESTFILE"
+    THIS_ERROR=0
     BASENAME=$(echo $TESTFILE|sed 's/\.oo$//')
+
+    if [ -e $BASENAME.skip ]; then
+        echo "Skipping $BASENAME"
+        continue
+    fi
+    echo "Testing $TESTFILE"
 
     INPUT=$TESTFILE
     OUTPUT=$TEMP_OE
@@ -89,45 +103,49 @@ for TESTFILE in $(find $SOURCE_FOLDER/* -name '*.oo'); do
     RET=$?
     set -e
 
-    if [ -e $BASENAME.oe ]; then
-        if [ $RET -ne 0 ]; then
-            echo "ERROR: $TESTFILE failed to link; expected success."
-            echo "Command: make build && $COMMAND $ARGS"
-            ERROR=1
-        elif ! diff -q $BASENAME.oe $TEMP_OE > /dev/null; then
-            echo "ERROR: $TESTFILE did not match expected $BASENAME.oe"
-            echo "Command: make build && $COMMAND $ARGS"
-            ERROR=1
-        fi
-    else
+    # check link status and object code
+    if [ $RET -eq 125 ]; then
+        echo "ERROR: linker crashed on $BASENAME; expected success or error message."
+        cat $TEMP_STDERR
+        THIS_ERROR=1
+    elif [ -e $BASENAME.fail ]; then
         if [ $RET -eq 0 ]; then
-            echo "ERROR: $TESTFILE linking succeeded; expected error."
-            echo "Command: make build && $COMMAND $ARGS"
-            ERROR=1
+            echo "ERROR: $BASENAME linking succeeded; expected error."
+            THIS_ERROR=1
         fi
+    elif [ $RET -ne 0 ]; then
+        echo "ERROR: $BASENAME linking failed; expected success."
+        THIS_ERROR=1
+    elif [ -e $BASENAME.oe ] && ! diff -q $BASENAME.oe $TEMP_OE > /dev/null; then
+        echo "ERROR: $BASENAME did not match expected $BASENAME.oe"
+        THIS_ERROR=1
     fi
 
-    if [ $ERROR = 0 ] && [ -e $BASENAME.stdout ]; then
+    # if successful, run
+    if [ $THIS_ERROR -ne 1 ] && [ -e $BASENAME.stdout ]; then
         set +e
         onrampvm $TEMP_OE > $TEMP_STDOUT
         RET=$?
         set -e
         if [ $RET -ne 0 ]; then
             echo "ERROR: $TESTFILE failed to run."
-            echo "Command: make build && $COMMAND $ARGS && onrampvm $TEMP_OE"
             ERROR=1
         elif ! diff $TEMP_STDOUT $BASENAME.stdout > /dev/null; then
             echo "ERROR: $TESTFILE output did not match expected $BASENAME.stdout"
-            echo "Command: make build && $COMMAND $ARGS && onrampvm $TEMP_OE"
             ERROR=1
         fi
+    fi
+
+    if [ $THIS_ERROR -eq 1 ]; then
+        echo "Command: make build && $COMMAND $ARGS && onrampvm $TEMP_OE"
+        ANY_ERROR=1
     fi
 
     rm -f $TEMP_OE
     rm -f $TEMP_STDOUT
 done
 
-if [ $ERROR -eq 1 ]; then
+if [ $ANY_ERROR -eq 1 ]; then
     echo "Errors occurred."
     exit 1
 fi
