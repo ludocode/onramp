3bd87918f6bc20c946de577d1864e6bb15ec3c0a
Nicholas Fraser <nick@ludocode.com>
2026-02-23 21:14:55 -0500

    libc/0: Fix memmove()

--- test/libc/0-oo/string/test_memmove.c
+++ test/libc/0-oo/string/test_memmove.c
@@ -7,11 +7,9 @@
 
 int main(void) {
 
-    // TODO compiler doesn't have support for this yet
-    /*
     // move down
     {
-        char bytes[] = {"Hello world"};
+        char bytes[] = "Hello world";
         memmove(bytes + 5, bytes + 8, 4);
         if (0 != strcmp(bytes, "Hellorld")) {
             return 1;
@@ -20,13 +18,12 @@ int main(void) {
 
     // move up
     {
-        char bytes[] = {"Hello world"};
+        char bytes[] = "Hello world";
         memmove(bytes + 3, bytes, 4);
         if (0 != strcmp(bytes + 3, "Hellorld")) {
             return 1;
         }
     }
-    */
 
     return 0;
 }
